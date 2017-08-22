a111107d93089607d1bcc82ec2359c7d76252521
caesar <dst910630@163.com>
2017-08-22 11:31:37 +0800

    学习资料

--- src/main/java/com/example/config/shiro/MyShiroRealm.java
+++ src/main/java/com/example/config/shiro/MyShiroRealm.java
@@ -1,26 +1,26 @@
 //package com.example.config.shiro;//package com.example.shiro;
-////
-////import org.apache.shiro.authc.AuthenticationException;
-////import org.apache.shiro.authc.AuthenticationInfo;
-////import org.apache.shiro.authc.AuthenticationToken;
-////import org.apache.shiro.realm.Realm;
-////
-/////**
-//// * Created by git on 2017/8/17.
-//// */
-////public class MyShiroRealm implements Realm {
-////    @Override
-////    public String getName() {
-////        return null;
-////    }
-////
-////    @Override
-////    public boolean supports(AuthenticationToken token) {
-////        return false;
-////    }
-////
-////    @Override
-////    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-////        return null;
-////    }
-////}
+//
+//import org.apache.shiro.authc.AuthenticationException;
+//import org.apache.shiro.authc.AuthenticationInfo;
+//import org.apache.shiro.authc.AuthenticationToken;
+//import org.apache.shiro.realm.Realm;
+//
+///**
+// * Created by git on 2017/8/17.
+// */
+//public class MyShiroRealm implements Realm {
+//    @Override
+//    public String getName() {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean supports(AuthenticationToken token) {
+//        return false;
+//    }
+//
+//    @Override
+//    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+//        return null;
+//    }
+//}
