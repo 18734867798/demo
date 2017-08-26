c4e2c30030e9885fbae59a1a75fabdfcf95b2600
caesar <dst910630@163.com>
2017-08-27 01:27:38 +0800

    修改web日志达到记录请求与响应

---
+++ src/main/java/com/example/config/log/LogRecordAspect.java
@@ -0,0 +1,76 @@
+package com.example.config.log;
+
+import com.alibaba.druid.support.json.JSONUtils;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.json.GsonJsonParser;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * Created by think on 2017/8/27.
+ */
+@Aspect   //定义一个切面
+@Configuration
+public class LogRecordAspect {
+    private  Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    // 定义切点Pointcut
+    @Pointcut("execution(public * com.example.controller..*.*(..))")
+    public void excudeService() {
+    }
+
+    @Around("excudeService()")
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        HttpServletRequest request = sra.getRequest();
+        HttpServletResponse response = sra.getResponse();
+
+        String url = request.getRequestURL().toString();
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        String queryString = request.getQueryString();
+
+        logger.info("..............................请求开始..............................");
+
+        logger.info("请求时间 : "+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()).toString());
+
+        logger.info("请求URL : " + request.getRequestURL().toString());
+
+        logger.info("请求参数 : " + Arrays.toString(pjp.getArgs()));
+
+        //logger.info("请求URI : " + request.getRequestURI().toString());
+
+        logger.info("请求方式 : " + request.getMethod());
+
+        logger.info("IP地址 : " + request.getRemoteAddr());
+
+        logger.info("请求对应执行controller方法的全路径 : " + pjp.getSignature().getDeclaringTypeName() + "." + pjp.getSignature().getName());
+
+
+        // result的值就是被拦截方法的返回值
+        Object result = pjp.proceed();
+
+
+        logger.info("controller的返回值是 " + new Gson().toJson(result));
+        logger.info("响应状态码 : "+ response.getStatus());
+        logger.info("..............................请求结束..............................");
+        return result;
+    }
+}
--- src/main/java/com/example/config/log/WebLogAspect.java
+++ src/main/java/com/example/config/log/WebLogAspect.java
@@ -1,128 +1,128 @@
-package com.example.config.log;
-
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Enumeration;
-
-/**
- * Created by git on 2017/8/18.
- */
-@Aspect
-
-@Component
-
-@Order(-5)
-public class WebLogAspect {
-    private Logger logger =  LoggerFactory.getLogger(this.getClass());
-
-    /**
-
-     * 定义一个切入点.
-
-     * 解释下：
-
-     * ~ 第一个 * 代表任意修饰符及任意返回值.
-
-     * ~ 第二个 * 任意包名
-
-     * ~ 第三个 * 代表任意方法.
-
-     * ~ 第四个 * 定义在web包或者子包
-
-     * ~ 第五个 * 任意方法
-
-     * ~ .. 匹配任意数量的参数.
-
-     */
-
-    @Pointcut("execution(public * com.example.controller..*.*(..))")
-
-    public void webLog(){}
-
-
-
-    @Before("webLog()")
-
-    public void doBefore(JoinPoint joinPoint){
-
-
-        // 接收到请求，记录请求内容
-
-        logger.info("WebLogAspect.doBefore()");
-
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-
-        HttpServletRequest request = attributes.getRequest();
-        HttpServletResponse response = attributes.getResponse();
-
-
-        // 记录下请求内容
-
-//        logger.info("URL : " + request.getRequestURL().toString());
+//package com.example.config.log;
 //
-//        logger.info("HTTP_METHOD : " + request.getMethod());
+//import org.aspectj.lang.JoinPoint;
+//import org.aspectj.lang.annotation.AfterReturning;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.annotation.Before;
+//import org.aspectj.lang.annotation.Pointcut;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.context.request.RequestContextHolder;
+//import org.springframework.web.context.request.ServletRequestAttributes;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.text.SimpleDateFormat;
+//import java.util.Arrays;
+//import java.util.Date;
+//import java.util.Enumeration;
+//
+///**
+// * Created by git on 2017/8/18.
+// */
+//@Aspect
+//
+//@Component
+//
+//@Order(-5)
+//public class WebLogAspect {
+//    private Logger logger =  LoggerFactory.getLogger(this.getClass());
+//
+//    /**
+//
+//     * 定义一个切入点.
+//
+//     * 解释下：
+//
+//     * ~ 第一个 * 代表任意修饰符及任意返回值.
+//
+//     * ~ 第二个 * 任意包名
+//
+//     * ~ 第三个 * 代表任意方法.
+//
+//     * ~ 第四个 * 定义在web包或者子包
+//
+//     * ~ 第五个 * 任意方法
+//
+//     * ~ .. 匹配任意数量的参数.
+//
+//     */
+//
+//    @Pointcut("execution(public * com.example.controller..*.*(..))")
+//
+//    public void webLog(){}
+//
+//
+//
+//    @Before("webLog()")
+//
+//    public void doBefore(JoinPoint joinPoint){
+//
+//
+//        // 接收到请求，记录请求内容
+//
+//        logger.info("WebLogAspect.doBefore()");
+//
+//        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+//
+//        HttpServletRequest request = attributes.getRequest();
+//        HttpServletResponse response = attributes.getResponse();
+//
+//
+//        // 记录下请求内容
+//
+////        logger.info("URL : " + request.getRequestURL().toString());
+////
+////        logger.info("HTTP_METHOD : " + request.getMethod());
+////
+////        logger.info("IP : " + request.getRemoteAddr());
+////
+////        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+////
+////        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+////
+////        logger.info("REQUEST_TIME:"+ new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()).toString());
+//
+//
+//        logger.info("请求URL : " + request.getRequestURL().toString());
+//
+//        logger.info("请求方式 : " + request.getMethod());
 //
 //        logger.info("IP : " + request.getRemoteAddr());
 //
-//        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+//        logger.info("请求对应执行controller方法的全路径 : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
 //
-//        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+//        logger.info("请求参数 : " + Arrays.toString(joinPoint.getArgs()));
 //
-//        logger.info("REQUEST_TIME:"+ new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()).toString());
-
-
-        logger.info("请求URL : " + request.getRequestURL().toString());
-
-        logger.info("请求方式 : " + request.getMethod());
-
-        logger.info("IP : " + request.getRemoteAddr());
-
-        logger.info("请求对应执行controller方法的全路径 : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-
-        logger.info("请求参数 : " + Arrays.toString(joinPoint.getArgs()));
-
-        logger.info("请求时间 : "+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()).toString());
-
-        logger.info("响应状态码 : "+ response.getStatus());
-
-        //logger.info("响应内容:"+response.);
-
-        //获取所有参数方法一：
-
-        Enumeration<String> enu=request.getParameterNames();
-
-        while(enu.hasMoreElements()){
-
-            String paraName=(String)enu.nextElement();
-
-            System.out.println(paraName+": "+request.getParameter(paraName));
-
-        }
-
-    }
-
-    @AfterReturning("webLog()")
-
-    public void  doAfterReturning(JoinPoint joinPoint){
-
-        // 处理完请求，返回内容
-
-        logger.info("WebLogAspect.doAfterReturning()");
-
-    }
-}
+//        logger.info("请求时间 : "+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()).toString());
+//
+//        logger.info("响应状态码 : "+ response.getStatus());
+//
+//        //logger.info("响应内容:"+joinPoint.);
+//
+//        //获取所有参数方法一：
+//
+//        Enumeration<String> enu=request.getParameterNames();
+//
+//        while(enu.hasMoreElements()){
+//
+//            String paraName=(String)enu.nextElement();
+//
+//            System.out.println(paraName+": "+request.getParameter(paraName));
+//
+//        }
+//
+//    }
+//
+//    @AfterReturning("webLog()")
+//
+//    public void  doAfterReturning(JoinPoint joinPoint){
+//
+//        // 处理完请求，返回内容
+//
+//        logger.info("WebLogAspect.doAfterReturning()");
+//
+//    }
+//}
