6789a8d61b5b04ac6268e2e3da2704c86da7d354
caesar <dst910630@163.com>
2017-08-27 18:51:22 +0800

    前进了一点

--- src/main/java/com/example/controller/PeopleController.java
+++ src/main/java/com/example/controller/PeopleController.java
@@ -25,16 +25,22 @@ public class PeopleController {
 
     @Autowired
     private PeopleMapper peopleMapper;
+
     @RequestMapping(value ="/findAll/{pageNum}/{pageSize}/{age}", method = RequestMethod.GET)
     public PageInfo findAll(@PathVariable(name = "pageNum")  String  pageNum, @PathVariable(name = "pageSize") String  pageSize, @PathVariable(name = "age") String age){
+
         pageNum = pageNum == null ? "1" : pageNum;
         pageSize = pageSize == null ? "3" : pageSize;
         age = age ==null ? null : age;
+        //1设置起始页，和每页大小
         PageHelper.startPage(new Integer(pageNum), new Integer(pageSize));
+        //2设置查询条件
         PeopleExample peopleExample=new PeopleExample();
         PeopleExample.Criteria criteria = peopleExample.createCriteria();
         criteria.andAgeEqualTo(age);
+        //3调用逆向工程生成代码
         List<People> list = peopleMapper.selectByExample(peopleExample);
+        //4将查询到的条件集合传给PageInfo
         PageInfo pageInfo = new PageInfo(list);
         Page page = (Page) list;
         return pageInfo;
